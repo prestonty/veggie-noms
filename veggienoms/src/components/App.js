c8816aecdea5df483f7031d060d76b54b2ef48df
hailey <hhaileychhan@gmail.com>
2023-05-06 22:10:56 -0400

    i added a profile (so cool!)

--- veggienoms/src/components/App.js
+++ veggienoms/src/components/App.js
@@ -10,7 +10,8 @@ function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
-      <Home></Home>
+      
+      <Profile></Profile>
     </div>
   );
 }
--- veggienoms/src/pages/profile.js
+++ veggienoms/src/pages/profile.js
@@ -1,9 +1,31 @@
+import '../styles/profile.css';
 import React from "react";
+import ProfileIcon from "../assets/logos/profile.png";
+import Submit from "../assets/buttons/submit.png";
 
 function Profile() {
   return (
-    <div className="profile">
-      <h1>profile</h1>
+    <div>
+    <div class="inline left-alignment">
+        <img class="splash" src={ProfileIcon} />
+    </div>
+    <div class="inline">
+    <h1 class="inline green top-text right-alignment">about you</h1>
+    <div class="inline">
+    <h2 class="dark">height (in)</h2>
+    <input type="text" min="0" max="96"/>
+    <h2 class="dark">weight (lbs)</h2>
+    <input type="text" min="0" max="600"/>
+    </div>
+    <div class="inline right-alignment float-right less-top-text">
+    <h2 class="dark">age</h2>
+    <input type="text" min="0" max="120"/>
+    <h2 class="dark">gender</h2>
+    <input type="checkbox" id="genderCheckbox"></input>
+    <label for="genderCheckbox"></label>
+    </div>
+    <img class="button" src={Submit}></img>
+    </div>
     </div>
   );
 }
